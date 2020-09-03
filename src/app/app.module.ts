a0fa6047b2d7228c4b003630a071979410a4d005
Adrian Lee <adrianlee0118@gmail.com>
2020-09-02 21:32:58 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayTextComponent } from './display-text/display-text.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayTextComponent,
+    TopBarComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-text/display-text.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-text',
+  templateUrl: './display-text.component.html',
+  styleUrls: ['./display-text.component.scss']
+})
+export class DisplayTextComponent implements OnInit {
+  word: String = "blazinga"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
